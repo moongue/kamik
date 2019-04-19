321bb6b796e6e8a7924232cc15042150d03f0835
Sasha <Wotsashavfhnnhfrnjhpfzw200110>
2019-04-19 19:49:10 +0300

    add

--- js/script.js
+++ js/script.js
@@ -12,11 +12,25 @@ function slider () {
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        speed: 1000,
+        speed: 1000
     });
-};
+}
+
+function sliderVideo() {
+    $(function () {
+        $('.slider-video').slick({
+            autoplay: false
+
+        });
+
+        $(".slider-video").on('afterChange', function (event, slick, currentSlide) {
+            $("#cp").text(currentSlide + 1);
+        });
+    });
+}
 
 $(window).on('load', function() {
     showMenu();
     slider();
+    sliderVideo();
 });
